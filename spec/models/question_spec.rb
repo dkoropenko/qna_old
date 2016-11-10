2f08654046d2354d9c4b72a3368571d348ad4772
smalew <nikotin1388@gmail.com>
2016-11-10 21:26:59 +0300

    Добавил зависимость delete_all для answers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   describe "Associations" do
-    it {should have_many :answers}
+    it {should have_many(:answers).dependent(:delete_all)}
     it {should belong_to :user}
   end
 
