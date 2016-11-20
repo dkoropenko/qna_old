ef2e0b01a837e5f94e25ad7a531ecf42ee260576
smalew <nikotin1388@gmail.com>
2016-11-20 18:41:48 +0300

    fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
 
-  accepts_nested_attributes_for :answers
   validates :title, :body, :user_id, presence: true
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 10 }
