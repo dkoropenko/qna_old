8d2134d9666617a2e5d9d50eb6a484a0782eb2f0
smalew <nikotin1388@gmail.com>
2016-11-23 00:38:35 +0300

    Complete homework

--- spec/features/answers/create_answer_spec.rb
+++ spec/features/answers/create_answer_spec.rb
@@ -6,33 +6,58 @@ feature 'Create new Answer', %q{
   He can write new answer for question
 } do
 
-  given(:user) { create :user }
-  given(:question) { create :question }
+  given!(:user) { create :user }
+  given!(:question) { create :question }
 
-  scenario 'Authenticated user create new answer for questions' do
-    sign_in user
+  scenario 'Authenticated user create new answer for questions', js: true do
+    sign_in user    
+    expect(page).to have_current_path root_path
 
     visit question_path question
-    fill_in 'Body', with: 'New answer body'
+
+    fill_in 'answer_body', with: 'New answer body'
     click_button 'Create answer'
 
     expect(page).to have_current_path question_path question
-    expect(page).to have_text 'New answer body'
+    within '.answers' do
+      expect(page).to have_text 'New answer body'
+    end
   end
+
   scenario 'Non authenticated user can not create new answer for questions' do
     visit question_path question
 
+    expect(page).to have_current_path(question_path question)
     expect(page).to_not have_selector :link_or_button, 'Create answer'
   end
 
-  scenario 'User try create new answer without valid data' do
-    sign_in user
+  scenario 'User try create new answer with empty data', js: true do
+    sign_in user    
+    expect(page).to have_current_path root_path
 
     visit question_path question
-    fill_in 'Body', with: ''
+
+    fill_in 'answer_body', with: ''
     click_button 'Create answer'
 
     expect(page).to have_current_path question_path question
-    expect(page).to have_text 'Answer body'
+    within '.answer_form' do
+      expect(page).to have_content "Body can\'t be blank"
+    end
+  end
+
+  scenario 'User try create new answer with short data', js: true do
+    sign_in user    
+    expect(page).to have_current_path root_path
+
+    visit question_path question
+
+    fill_in 'answer_body', with: '123456'
+    click_button 'Create answer'
+
+    expect(page).to have_current_path question_path question
+    within '.answer_form' do
+      expect(page).to have_content 'Body is too short'
+    end
   end
 end
